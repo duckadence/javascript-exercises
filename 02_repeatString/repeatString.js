6c6c251b28a935be0f79efe9b76f0b01074c3220
Jonathan Lin <jonlin11@gmail.com>
2022-01-08 00:54:24 +0000

    finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0)
+    {
+        return "ERROR";
+    }
 
+    let temp = "";
+
+    for (let count = 0; count < num; count++)
+    {
+        temp += string;
+    }
+    return temp;
 };
 
 // Do not edit below this line
