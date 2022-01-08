365f72cd0941b35b1ea7608b88b52b906b329780
Jonathan Lin <jonlin11@gmail.com>
2022-01-08 05:14:26 +0000

    finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
 
+    let tempC;
+
+    tempC = (tempF - 32) * (5/9);
+
+    tempC = Math.round(tempC * 1e1) / 1e1;
+
+    return tempC;
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
 
+    let tempF;
+
+    tempF = (tempC * (9/5)) + 32;
+    
+    tempF = Math.round(tempF * 1e1) / 1e1;
+
+    return tempF;
 };
 
 // Do not edit below this line
