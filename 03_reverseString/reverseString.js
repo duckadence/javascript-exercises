809bc584e3e065c7500e030f86e507f3c892d42f
Jonathan Lin <jonlin11@gmail.com>
2022-01-08 01:59:18 +0000

    finished 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    let arr = str.split("");
+    let reverseStr = "";
 
+    for (let count = arr.length - 1; count >= 0; count--)
+    {
+        reverseStr += arr[count];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
