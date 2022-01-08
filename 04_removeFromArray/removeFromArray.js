ede212414686fc910b58dec49121a2f01d75732a
Jonathan Lin <jonlin11@gmail.com>
2022-01-08 04:33:06 +0000

    finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, e1, e2, e3, e4, e5) {
+    let tempArr = [];
+    let tempElement;
+    let index = 0;
 
+    for (let count = 0; count < arr.length; count++)
+    {
+        tempElement = arr[count];
+        if (tempElement === e1 || tempElement === e2 || tempElement === e3 || tempElement === e4 || tempElement === e5)
+        {
+            continue;
+        }
+        else
+        {
+            tempArr[index] = tempElement;
+            index++;
+        }
+    }
+
+    return tempArr;
 };
 
 // Do not edit below this line
