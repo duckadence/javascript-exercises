61ecaf58b67fda2a779cdb952e571a082f0a84b1
Jonathan Lin <jonlin11@gmail.com>
2022-01-08 04:48:42 +0000

    finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0)
+    {
+        let sum = 0;
 
+        if (num1 > num2)
+        {
+            first = num2;
+            second = num1;
+        }
+        else
+        {
+            first = num1;
+            second = num2;
+        }
+
+        for (let count = first; count <= second; count++)
+        {
+            sum += count;
+        }
+        return sum;
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
