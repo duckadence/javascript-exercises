d591fb3dee72910be935c972fb9cf6b6b8a85fea
Jonathan Lin <jonlin11@gmail.com>
2022-01-17 19:07:50 +0000

    finished 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let sum = 0;
+	for(let count = 0; count < arr.length; count++)
+    {
+        sum += arr[count];
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    let product = 1
+	for(let count = 0; count < arr.length; count++)
+    {
+        product *= arr[count];
+    }
+    return product;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+    let result = num;
+	for (let count = 1; count < exp; count++)
+    {
+        result *= num;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+    for (let count = num; count > 0; count--)
+    {
+        factorial *= count;
+    }
+    return factorial;
 };
 
 // Do not edit below this line
