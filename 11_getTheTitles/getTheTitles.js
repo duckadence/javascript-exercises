4307e819a0a6118757ea8d89a8513bbe6ce86ec7
Jonathan Lin <jonlin11@gmail.com>
2022-01-17 23:02:02 +0000

    completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
 
+let arrTitle = [];
+
+for (let count = 0; count < arr.length; count++)
+{
+    arrTitle.push(arr[count].title);
+}
+
+return arrTitle;
 };
 
 // Do not edit below this line
