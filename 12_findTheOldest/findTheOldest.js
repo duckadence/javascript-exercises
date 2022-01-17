0b1fd135035ccdbb0da4ff713005c2ba2abd239a
Jonathan Lin <jonlin11@gmail.com>
2022-01-17 23:32:45 +0000

    finished 12_findTheOldest ls

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+let oldestAge = 0;
+let oldestIndex = 0;
+
+for (let count = 0; count < arr.length; count++)
+{
+    if (arr[count].yearOfDeath == null)
+    {
+        arr[count].yearOfDeath = 2022;
+    }
+    if (oldestAge < arr[count].yearOfDeath - arr[count].yearOfBirth)
+    {
+        oldestAge = arr[count].yearOfDeath - arr[count].yearOfBirth;
+        oldestIndex = count;
+    }
+}
+
+return arr[oldestIndex];
 };
 
 // Do not edit below this line
