73b581c886dcf346a55a09f3b8fe2228967b8a77
Jonathan Lin <jonlin11@gmail.com>
2022-01-17 21:17:20 +0000

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z]/g, '');
 
+    let arr = str.split('');
+
+    for (let front = 0, back = arr.length - 1; front < back; front++, back--)
+    {
+        if (arr[front] != arr[back])
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
