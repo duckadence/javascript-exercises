64c7c840ae6e1dc8c3d5ab916d0800549d9c60d7
Jonathan Lin <jonlin11@gmail.com>
2022-01-17 22:57:11 +0000

    finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+if (num < 0)
+{
+    return 'OOPS';
+}
+
+let num1 = 1;
+let num2 = 1;
+let temp;
+
+for (let count = num; count > 1; count--)
+{
+    temp = num2;
+    num2 += num1;
+    num1 = temp;
+}
+
+return num1;
 };
 
 // Do not edit below this line
